bca158f7efce9e468dcf406738532ecbbe222662
Alexander Jaramillo <alexunjm@gmail.com>
2023-12-07 22:40:37 -0500

    drawing board

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ const context = canvas.getContext('2d')
 const BLOCK_SIZE = 20
 const BOARD_WIDTH = 14
 const BOARD_HEIGHT = 30
+const COLOR = {
+  SOLID: '#02D'
+}
 
 canvas.width = BOARD_WIDTH * BLOCK_SIZE
 canvas.height = BOARD_HEIGHT * BLOCK_SIZE
 
 context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// board
+const board = [
+  ...Array(BOARD_HEIGHT - 1).fill(Array(BOARD_WIDTH).fill(0)),
+  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
 // game loop
 function update () {
@@ -22,5 +31,15 @@ function update () {
 function draw () {
   context.fillStyle = '#444'
   context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = COLOR.SOLID
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
 }
+
 update()
